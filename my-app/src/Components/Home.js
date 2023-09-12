9e859007960e0f0f691255f6e340b816aca76424
alreicha <areichard@haverford.edu>
2023-09-11 22:09:05 -0400

    all changes for access token approval

---
+++ my-app/src/Components/Home.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+function Home() {
+  useEffect(() => {
+    const url = window.location.href;
+    const hasCode = url.includes("?code=");
+
+    if (hasCode) {
+      const newUrl = new URL(url);
+      const AUTH_CODE = newUrl.searchParams.get("code");
+      fetchAccessToken(AUTH_CODE);
+    }
+  }, []);
+
+  const fetchAccessToken = (code) => {
+    // Fetch access token from your server, assuming you have set up a server endpoint to exchange the code for a token
+    axios.get(`/server/endpoint?code=${code}`)
+    .then(res => {
+        // Here you'll get the access token, store it for further use
+        const accessToken = res.data.access_token;
+        console.log("Access Token: ", accessToken);
+    })
+    .catch(err => console.log(err));
+  };
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ my-app/src/Components/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+
+function Login() {
+  const redirectToSpotify = () => {
+    // Replace these with your client ID and redirect URI
+    const CLIENT_ID = 'f96c84ccf962498b8499d78509c90ebf';
+    const REDIRECT_URI = 'http://localhost:3004/home'; 
+    
+    const authURL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=user-top-read`;
+    
+    window.location.href = authURL;
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={redirectToSpotify}>Login with Spotify</button>
+    </div>
+  );
+}
+
+export default Login;
+
