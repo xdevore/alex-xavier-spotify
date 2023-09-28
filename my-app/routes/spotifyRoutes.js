1ef33c0c96a2b4a2b56a16ff55a8d452868cc3a2
alreicha <areichard@haverford.edu>
2023-09-27 21:40:48 -0400

    Made a bunch of files and started to house the db stream

--- my-app/routes/spotifyRoutes.js
+++ my-app/routes/spotifyRoutes.js
@@ -1,30 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
-const CLIENT_ID = 'f96c84ccf962498b8499d78509c90ebf';
-const CLIENT_SECRET = 'b71ec3ea5e174d2daf3653d02fe9a620';
+const spotifyController = require('../controllers/SpotifyAPI/spotifyAccess.js'); 
 
-router.post('/get-token', async (req, res) => {
-    const authCode = req.body.code;
-    
-    try {
-        const response = await axios({
-            method: 'post',
-            url: 'https://accounts.spotify.com/api/token',
-            data: `grant_type=authorization_code&code=${authCode}&redirect_uri=http://localhost:3000/login`,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + (new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
-            },
-        });
-        
-        const accessToken = response.data.access_token;
-        res.json({ access_token: accessToken });
-
-    } catch (error) {
-        console.error('Error fetching the Spotify access token:', error.response ? error.response.data : error.message);
-        res.status(500).send('Error fetching the Spotify access token.');
-    }
-});
+router.post('/get-token', spotifyController.getToken);
 
 module.exports = router;
